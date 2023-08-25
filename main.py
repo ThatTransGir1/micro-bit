17f9178e1ff3a894a7af59ab3986f125f2f348b9
ThatTransGir1 <143089303+ThatTransGir1@users.noreply.github.com>
2023-08-25 11:07:07 +0000

    created simple micro:bit program

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from microbit import *
-
+import random
+import microbit as mb
+print('Starting Program')
+while True:
+    mb.display.clear
+    #temp = mb.temperature()
+    #mb.display.scroll(temp)
+    if mb.button_a.is_pressed() is True:
+        print('Button A Pressed')
+        mb.display.set_pixel(1,1,5)
+        mb.display.scroll('Hello World',loop=False)
