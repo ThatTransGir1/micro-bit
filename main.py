788b23e8c47d859d3b4b0ef4a806b9f35817c71e
ThatTransGir1 <143089303+ThatTransGir1@users.noreply.github.com>
2023-08-25 11:56:55 +0000

    made changes to program

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import random
+import sys
 import microbit as mb
 print('Starting Program')
 while True:
     mb.display.clear
     #temp = mb.temperature()
-    #mb.display.scroll(temp)
+    #mb.display.scroll(temp,loop=False)
     if mb.button_a.is_pressed() is True:
         print('Button A Pressed')
         mb.display.set_pixel(1,1,5)
         mb.display.scroll('Hello World',loop=False)
+    if mb.button_b.is_pressed() is True:
+        print('Button B Pressed')
+        ranNum = random.random()
+        mb.display.scroll(ranNum,loop=False)
+        #sys.exit(mb.display.scroll('Exit: 1'))
